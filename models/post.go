52138f59168da0d6d495ba4a971d43e0fc80813f
Mark Raiter <raitermrk@gmail.com>
2023-06-09 00:58:30 +0300

    switched echo to gin

--- models/post.go
+++ models/post.go
@@ -1,7 +1,7 @@
 package models
 
 type Post struct {
-	UserID int    `json:"userId"`
+	UserID int    `json:"userId" db:"user_id"`
 	Title  string `json:"title"`
 	Body   string `json:"body"`
 }
--- pkg/repository/post_postgres.go
+++ pkg/repository/post_postgres.go
@@ -20,9 +20,9 @@ func NewPostPostgres(db *sqlx.DB) *PostPostgres {
 
 func (r *PostPostgres) Create(userID int, post models.Post) (int, error) {
 	var id int
-	createPostQuery := fmt.Sprintf("INSERT INTO %s (userId, title, body) VALUES ($1, $2, $3) RETURNING id", postsTable)
+	createPostQuery := fmt.Sprintf("INSERT INTO %s (user_id, title, body) VALUES ($1, $2, $3) RETURNING id", postsTable)
 
-	row := r.db.QueryRow(createPostQuery, post.Title, post.Body)
+	row := r.db.QueryRow(createPostQuery, post.UserID, post.Title, post.Body)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
